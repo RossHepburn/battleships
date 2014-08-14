21484d4866043f77d5cbfbb97130b70328479996
rosshepburn <hepburnjrl@gmail.com>
2014-08-14 19:19:44 +0100

    Can add a new player

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,8 +3,8 @@ require 'player'
 describe Player do
 	
 	it "should be able to add a player" do
-		player = Player.new("joe")
-		expect(player.player_name).to eq("joe")
+		player = Player.new("Ross")
+		expect(player.player_name).to eq("Ross")
 	end	
 
 
